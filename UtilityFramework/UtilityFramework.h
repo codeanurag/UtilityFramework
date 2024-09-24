a8bd72554f0c9dbdb18cfe323a5d1764d7a422e6
Anurag Pandit <codeanuragpandit@gmail.com>
2024-09-24 20:04:27 +0530

    Initial Commit

---
+++ UtilityFramework/UtilityFramework.h
@@ -0,0 +1,18 @@
+//
+//  UtilityFramework.h
+//  UtilityFramework
+//
+//  Created by Anurag Pandit on 24/09/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UtilityFramework.
+FOUNDATION_EXPORT double UtilityFrameworkVersionNumber;
+
+//! Project version string for UtilityFramework.
+FOUNDATION_EXPORT const unsigned char UtilityFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UtilityFramework/PublicHeader.h>
+
+
